b6955cfef56c8dd82773228896ff7e556e9f3028
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-05-30 06:20:46 -0400

    Pull

--- initrd/add-to-initrd.sh
+++ initrd/add-to-initrd.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 
-BASE_DIR=$PWD/initrd
+BASE_DIR=$PWD/initramfs
+FILE=`realpath $1`
+
+if [ $# -lt 1 ]
+then
+	echo "Usage `basename $0` <source_initrd>"
+	exit 1
+fi
+
+rm -rf $BASE_DIR
 
 mkdir $BASE_DIR
 
@@ -14,11 +23,17 @@ gcc -o hello hello.c
 
 rm hello.c
 
-cp /boot/initramfs-`uname -r`.img initramfs.gz
+cp $FILE initramfs.gz
 
 #mv initramfs-`uname -r`.img initramfs.gz
-
-gunzip initramfs.gz
+if gzip -t initramfs.gz; 
+then
+	echo 'file is gzip archive'
+	gunzip initramfs.gz
+else 
+	echo 'file is not gzip assuming cpio archive'
+	mv initramfs.gz initramfs
+fi
 
 mkdir tmp
 
@@ -28,10 +43,15 @@ cpio -id < ../initramfs
 
 mv ../hello usr/bin/
 
+tree usr/bin
+
 find . | cpio --create --format='newc' > ../newinitramfs
 
 cd ..
 
 gzip newinitramfs
 
-mv newinitramfs.gz newinitramfs.img
+mv newinitramfs.gz ../newinitramfs.img
+
+
+rm -rf ../initramfs
